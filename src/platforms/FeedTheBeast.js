d857ffa0ebab0f0e883b4c54fe6ea9d43831ab8a
Bill Ewing <bill@nubs.io>
2021-02-12 14:37:41 -0600

    FTB works, probably

--- src/platforms/FeedTheBeast.js
+++ src/platforms/FeedTheBeast.js
@@ -1,87 +1,80 @@
 import Platform from './Platform';
-import XmlParser from 'fast-xml-parser';
+
+// See: https://github.com/FTBTeam/FTB-App
+// API Base URL: https://api.modpacks.ch
+// /public/modpack/:packID/:versionID?
+// /public/modpack/popular/installs/:limit
+// /public/modpack/:packID/:versionID
+// /public/modpack/:packID/:versionID/server/linux downloads a binary for CreeperHost's download tool. Do not mistake this as a potential useful route.
 
 class FeedTheBeastPlatform extends Platform {
 
   async fetch() {
-    const data = await ::this.request('static/modpacks.xml', {});
-    return XmlParser.parse(data);
+    const data = await ::this.request('all', {});
+    return data;
   }
 
   async process(data) {
     console.log(data);
-    if (typeof data === 'undefined' ||
-        typeof data.modpacks === 'undefined' ||
-        typeof data.modpacks.modpack === 'undefined') {
-      this.log('Encountered error while processing data: invalid data');
-      return null;
-    }
+    this.log(`Processing ${data?.packs?.length} packs`);
 
-    const packages = { };
+    const packages = {};
 
-    for (const originalPack of data.modpacks.modpack) {
-      originalPack = originalPack.$;
+    for (const packId of data?.packs) {
+      const pack = await ::this.request(`${packId}`);
 
-      if (this.ignoredPackages.includes(originalPack.dir)) {
-        this.log('Skipping ignored package: %s', originalPack.name);
-        continue;
-      }
-
-      // Skip if no server download exists
-      if (typeof originalPack.serverPack === 'undefined' ||
-          originalPack.serverPack === null ||
-          originalPack.serverPack.length === 0) {
-        this.log('Skipping pack with no server download: %s', originalPack.name);
-        continue;
-      }
-
-      var id =
-        originalPack.dir
-        .replace(/(FTBPresents|FTB|ftb_)/g, 'ftb-')
+      var id = pack.name
+        .replace(/(FTB Presents|FTB|ftb_)/g, 'ftb-')
         .replace(/([a-z])_([A-Z])/g, '$1-$2')
         .replace(/_(\d+)_(\d+)_(\d+)$/g, '-$1.$2.$3')
         .replace(/_(\d+)_(\d+)$/g, '-$1.$2')
         .replace(/_(\d)(\d)$/g, '-$1.$2')
         .replace(/([a-z])([A-Z])/g, '$1-$2')
         .replace(/([a-z])_(\d+)/g, '$1$2')
+        .replace(/['"]/g, '')
+        .replace(/[ ]+/g, '-')
+        .replace(/[-]{2}/g, '-')
         .toLowerCase();
 
       id = (id.substr(0, 4) !== 'ftb-' ? 'ftb-' + id.replace(/ftb-/g, '') : id);
 
-      var pattern = new RegExp(id + '(?:[.-]([\\d.]+))?\\.jar');
-
-      var currentVersion = originalPack.version;
-      const pack = {
-        versions: [],
-        name: originalPack.name,
-        source_ref: originalPack.name,
-      };
-
-      var versionNumbers = [];
-
-      if (originalPack.oldVersions) {
-        versionNumbers = originalPack.oldVersions.split(';');
+      if (typeof packages[pack.id] === 'undefined') {
+        packages[pack.id] = {
+          versions: [],
+          name: pack.name,
+          slug: id,
+          source_ref: pack.id
+        };
       }
 
-      if (versionNumbers[0] !== currentVersion) {
-        versionNumbers = [ currentVersion ].concat(versionNumbers);
-      }
+      for (const packVersion of pack?.versions) {
+        const version = pack.tags.find(o => o.name.match(/(1\.)([0-9]{1,2}\.)([0-9]+)?/))?.name;
+        const gameVer = await ::this.findGameVersion(this, version ?? '1.7.10');
+        if (!gameVer) continue;
 
-      versionNumbers.forEach(
-        function addVersions(version) {
-          if (version.length <= 0) return;
-
-          pack.versions.push(generateVersion(originalPack, version));
+        const pkg = {
+          package_id: this.id,
+          game_version_id: gameVer.id,
+  
+          name: `${packVersion.name}`,
+          version: `${packVersion.id}`,
+  
+          origin: '',
+          created_at: new Date(packVersion.updated * 1000)
+        };
+  
+        try {
+          const pkgEntry = this.packages[pack.id].versions.find(ep => `${ep.version}` === `${packVersion.name}`);
+          if (pkgEntry?.origin) continue;
+        } catch (err) {
+          // Do nothing. 
         }
-      );
 
-      packs.push(pack);
+        packages[pack.id].versions.push(pkg);
       }
+    }
 
-    return {
-      packages: packs,
-      meta: {}
-    };
+    return { packages, meta: {} };
   }
 }
 
